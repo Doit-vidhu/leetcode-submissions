0d6f3c490837323f49a858d092a3589369578b56
Doit-vidhu <rajvidhu97@gmail.com>
2026-03-18 06:48:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (48.07%)

--- submissions/2316-count-hills-and-valleys-in-an-array/solution.cpp
+++ submissions/2316-count-hills-and-valleys-in-an-array/solution.cpp
@@ -3,28 +3,15 @@ public:
     int countHillValley(vector<int>& nums) {
         int hill = 0;
         int valley = 0;
-        int n = nums.size();
-        vector<int> num1;
-        for (int i = 0; i<n-1; i++){
-            if(nums[i] != nums[i+1]){
-                num1.push_back(nums[i]);
-
-            }
-
-
-
-        }
-        num1.push_back(nums[n-1]);
-        for (int j=1; j<num1.size()-1; j++){
-            if (num1[j]>num1[j-1] && num1[j]>num1[j+1]){
-                hill++;
-            }
-            else if (num1[j]<num1[j-1] && num1[j]<num1[j+1]){
-                valley++;
-            }
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        
+        for (int i = 1; i<nums.size()-1; i++){
+            if(nums[i] > nums[i-1] && nums[i]> nums[i+1]) hill++;
+            else if(nums[i] < nums[i-1] && nums[i] < nums[i+1] ) valley++;
         }
         return (hill + valley);
 
+       
 
         
             
