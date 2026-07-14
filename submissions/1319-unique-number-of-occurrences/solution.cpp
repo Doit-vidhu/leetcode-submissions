3d4879b6ea312fccc962fa00e3b2ad918f7432d3
Doit-vidhu <rajvidhu97@gmail.com>
2026-07-14 21:18:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (46.70%)

--- submissions/1319-unique-number-of-occurrences/solution.cpp
+++ submissions/1319-unique-number-of-occurrences/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> v(2001,0);
-        for (int i : arr){
-            v[i+1000]++;  
-        }
-        v.erase(remove(v.begin(), v.end(), 0), v.end());
-        set<int> myset(v.begin(), v.end());
-        if(myset.size() == v.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        map<int, int> mpp;
+        set<int> st;
+        for(int i = 0; i<arr.size(); i++){
+            mpp[arr[i]]++;
 
-
-        
-        
+        }
+        for(auto& [key, value] : mpp){
+            st.insert(value);
+        }
+        if (mpp.size() == st.size()) return true;
+        else return false;
         
     }
 };
